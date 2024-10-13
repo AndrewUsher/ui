4f298823ccfb4216053ba45132c1df63b2de033e
Andrew Usher <andrewusher00@gmail.com>
2024-10-12 22:38:50 -0500

    chore: add library build support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'framer-motion']
+    }
+  }
 })
